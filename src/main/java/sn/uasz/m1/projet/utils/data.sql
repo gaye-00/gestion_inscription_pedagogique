0129e5e75135758fc4110f2c6804c4682fea62a2
gaye-00 <gayeabdoulaye163@gmail.com>
2025-03-18 23:56:28 +0000

    crypetage du mot de passe

--- src/main/java/sn/uasz/m1/projet/utils/data.sql
+++ src/main/java/sn/uasz/m1/projet/utils/data.sql
@@ -9,12 +9,12 @@ VALUES
 INSERT INTO utilisateur (nom, prenom, dateNaissance, ine, sexe, adresse, email, inscriptionValidee, password, role)
 VALUES 
 ('Ngor', 'Fall', '2000-05-15', "INE001", 'MASCULIN', 'Dakar', 'e', false, 'e', 'ETUDIANT'),
-('Gaye', 'Abdoulaye', '2000-05-15', "INE002", 'MASCULIN', 'Dakar', 'abdoulaye.gaye@example.com', false, 'password123', 'ETUDIANT'),
-('Diop', 'Aminata', '1998-08-22', "INE003", 'FEMININ', 'Thiès', 'aminata.diop@example.com', false, 'password456', 'ETUDIANT'),
-('Faye', 'Mamadou', '1999-12-10', "INE004", 'MASCULIN', 'Saint-Louis', 'mamadou.faye@example.com', false, 'password789', 'ETUDIANT'),
+('Gaye', 'Abdoulaye', '2000-05-15', "INE002", 'MASCULIN', 'Dakar', 'abdoulaye.gaye@example.com', false, SHA2('password123', 256), 'ETUDIANT'),
+('Diop', 'Aminata', '1998-08-22', "INE003", 'FEMININ', 'Thiès', 'aminata.diop@example.com', false, SHA2('password456', 256), 'ETUDIANT'),
+('Faye', 'Mamadou', '1999-12-10', "INE004", 'MASCULIN', 'Saint-Louis', 'mamadou.faye@example.com', false, SHA2('password789', 256), 'ETUDIANT'),
 ('Tamba', 'Mbaye', '2004-03-30', null, 'FEMININ', 'Tamba', 'r', null, 'r', 'ResponsablePedagogique'),
-('Sow', 'Fatou', '2001-03-30', null, 'FEMININ', 'Ziguinchor', 'fatou.sow@example.com', null, 'password321', 'ResponsablePedagogique'),
-('Ndoye', 'Cheikh', '1997-07-05', null, 'MASCULIN', 'Kaolack', 'cheikh.ndoye@example.com', null, 'password654', 'ResponsablePedagogique');
+('Sow', 'Fatou', '2001-03-30', null, 'FEMININ', 'Ziguinchor', 'fatou.sow@example.com', null, SHA2('password321', 256), 'ResponsablePedagogique'),
+('Ndoye', 'Cheikh', '1997-07-05', null, 'MASCULIN', 'Kaolack', 'cheikh.ndoye@example.com', null, SHA2('password654', 256), 'ResponsablePedagogique');
 
 INSERT INTO formation (nom, code, nombreOptionsRequis, niveau, responsable_id)
 VALUES 
@@ -36,10 +36,7 @@ VALUES
     ('INFO102', 'Bases de Données', 45, 1.0, 4, 'Fatou Diop', 1, 2, true),
     ('INFO103', 'Programmation Web', 30, 1.0, 4, 'Moussa Ndiaye', 1, 3, false),
     ('INFO201', 'Intelligence Artificielle', 60, 1.5, 6, 'Amadou Diallo', 2, 1, true),
-    ('INFO202', 'Big Data', 45, 1.0, 4, 'Fatou Diop', 2, 2, false);
-
-INSERT INTO ue (code, nom, volumeHoraire, coefficient, credits, nomResponsable, formation_id, enseignant_id, obligatoire)
-VALUES 
+    ('INFO202', 'Big Data', 45, 1.0, 4, 'Fatou Diop', 2, 2, false),
     ('INFO104', 'Anglais', 60, 1.5, 6, 'Mme Dabo', 1, 1, false),
     ('INFO105', 'Optimatisation', 45, 1.0, 4, 'Youssou Dieng', 1, 2, false),
     ('INFO106', 'Theorie des graphes', 30, 1.0, 4, 'Youssou Dieng', 1, 3, false);
@@ -53,35 +50,12 @@ VALUES
     (2, 'TP');
 
 
--- INSERT INTO utilisateur (nom, prenom, dateNaissance, sexe, adresse, email, password, role)
--- VALUES 
---     ('Fall', 'Awa', '2001-05-12', 'FEMININ', 'Dakar', 'awa.fall@etu.uasz.sn', '123456', 'ETUDIANT'),
---     ('Sow', 'Ibrahima', '2000-11-22', 'MASCULIN', 'Ziguinchor', 'ibrahima.sow@etu.uasz.sn', '654321', 'ETUDIANT'),
---     ('Ba', 'Mariama', '1999-02-10', 'FEMININ', 'Thiès', 'mariama.ba@etu.uasz.sn', '111222', 'ETUDIANT'),
---     ('Seck', 'Ousmane', '2001-08-30', 'MASCULIN', 'Kaolack', 'ousmane.seck@etu.uasz.sn', '333444', 'ETUDIANT');
-
--- INSERT INTO etudiant (ine, groupe_id, formation_id, inscriptionValidee, id)
--- VALUES 
---     ('INE001', 1, 1, true, 1),
---     ('INE002', 2, 1, false, 2),
---     ('INE003', 3, 2, true, 3),
---     ('INE004', 4, 2, false, 4);
-
-
 INSERT INTO etudiant_ue (etudiant_id, ue_id)
 VALUES 
     (1, 1), (1, 2),
     (2, 1), (2, 3),
     (3, 4), (3, 5),
     (4, 4);
-
-
--- INSERT INTO utilisateur (nom, prenom, dateNaissance, sexe, adresse, email, password, role)
--- VALUES 
---     ('Ndour', 'Abdou', '1980-07-15', 'MASCULIN', 'Saint-Louis', 'abdou.ndour@uasz.sn', 'admin123', 'ResponsablePedagogique');
-
--- INSERT INTO responsable_pedagogique (id)
--- VALUES (5);
 
 
 SELECT * FROM formation;
